ba4a1b38d853c2bd6d2533eceecdb2a478212433
nomancsediu <abdnoman720@gmail.com>
2026-05-12 20:30:39 +0600

    feat: add Redis setup

--- nomancsediu/chat-app/backend/user/dist/index.js
+++ nomancsediu/chat-app/backend/user/dist/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
+import { createClient } from 'redis';
 dotenv.config();
 connectDb();
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+redisClient.connect().then(() => console.log("Redis connected")).catch(console.error);
 const app = express();
 const port = process.env.PORT;
 app.listen(port, () => {
--- nomancsediu/chat-app/backend/user/src/index.ts
+++ nomancsediu/chat-app/backend/user/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv  from  'dotenv';
 import connectDb from './config/db.js';
+import {createClient} from 'redis';
 
 dotenv.config();
 
-connectDb();   
+connectDb();  
+
+export const redisClient = createClient({
+    url: process.env.REDIS_URL!,
+});
+
+redisClient.connect().then(() => console.log("Redis connected")).catch(console.error);
 
 const app = express();
 
