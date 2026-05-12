0f658f8cc6689f9d755bbcc76558bb6a06bb01e3
nomancsediu <abdnoman720@gmail.com>
2026-05-13 01:58:48 +0600

    feat(user): add OTP verification functionality

--- nomancsediu/chat-app/backend/user/src/controllers/user.ts
+++ nomancsediu/chat-app/backend/user/src/controllers/user.ts
@@ -2,7 +2,10 @@ import type { Request, Response, NextFunction } from "express"
 import TryCatch from "../config/TryCatch.js"
 import { redisClient } from "../index.js"
 import { publishToQueue } from "../config/rabitmq.js";
+import { User } from "../model/User.js";
+import { generateToken } from "../config/generateToken.js";
 
+// OTP Generation 
 export const loginUser = TryCatch(async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body
 
@@ -43,4 +46,56 @@ export const loginUser = TryCatch(async (req: Request, res: Response, next: Next
     success: true,
     message: "OTP sent successfully"
   });
+});
+
+
+//OTP Verification 
+export const verifyUser = TryCatch(async(req:Request, res:Response, next:NextFunction)=>
+{
+  const {email, otp:enteredOtp} = req.body
+
+  if(!email || !enteredOtp)
+  {
+    res.status(400).json({
+      message: "Email and OTP are required",
+    });
+    return;
+  }
+
+  const otpKey = `otp:${email}`
+
+  const storedOtp = await redisClient.get(otpKey);
+
+  if(!storedOtp || storedOtp != enteredOtp)
+  {
+    res.status(400).json({
+      success: false,
+      message: "Invalid or expired OTP",
+    });
+
+    return;
+  }
+
+  await redisClient.del(otpKey);
+
+  let user = await User.findOne({email})
+
+  if(!user)
+  {
+    const name = email.slice(0,8);
+    user = await User.create({
+      name,
+      email
+    });
+  }
+  else{
+    const token = generateToken(user);
+    res.json({
+      message:"User Verified",
+      token,
+      user,
+      success:true
+    })
+  }
+
 });
