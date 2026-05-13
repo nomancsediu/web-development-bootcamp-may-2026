cf71d9cfa56d46318601d21431f913e131ba8b47
nomancsediu <abdnoman720@gmail.com>
2026-05-13 17:54:11 +0600

    feat: add Chat and Message models

---
+++ nomancsediu/chat-app/backend/chat/src/models/Messages.ts
@@ -0,0 +1,58 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface IMessage extends Document {
+    chatId: Types.ObjectId;
+    sender: string;
+    text?: string;
+    image?: {
+        url: string;
+        publicId: string;
+    };
+    messageType: "text" | "image";
+    seen: boolean;
+    seenAt?: Date;
+    hiddenFrom: string[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const schema: Schema<IMessage> = new Schema(
+    {
+        chatId: {
+            type: Schema.Types.ObjectId,
+            ref: "Chat",
+            required: true,
+        },
+        sender: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+        },
+        image: {
+            url: String,
+            publicId: String,
+        },
+        messageType: {
+            type: String,
+            enum: ["text", "image"],
+            default: "text",
+        },
+        seen: {
+            type: Boolean,
+            default: false,
+        },
+        seenAt: {
+            type: Date,
+            default: null,
+        },
+        hiddenFrom: {
+            type: [String],
+            default: [],
+        },
+    },
+    { timestamps: true }
+);
+
+export const Message = mongoose.model<IMessage>("Messages", schema);
