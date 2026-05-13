93c373ef05f619ced71ccb500f4a49d49fe414e7
nomancsediu <abdnoman720@gmail.com>
2026-05-13 17:54:51 +0600

    feat: add chat routes with auth middleware

--- nomancsediu/chat-app/backend/chat/src/routes/chat.ts
+++ nomancsediu/chat-app/backend/chat/src/routes/chat.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createNewChat } from '../controllers/chat.js';
-
+import { createNewChat, getAllChats, getMessagesByChat, sendMessage, } from '../controllers/chat.js';
+import { isAuth } from '../middleware/isAuth.js';
+import { upload } from '../middleware/multer.js';
 
 const router = express.Router();
 
-
-router.post('/chat/new', createNewChat);
+router.post('/chat/new', isAuth, createNewChat);
+router.post('/chat/all', isAuth, getAllChats);
+router.post("/message", isAuth, upload.single('image'), sendMessage);
+router.get("/message/:chatId", isAuth, getMessagesByChat);
 
 export default router;
