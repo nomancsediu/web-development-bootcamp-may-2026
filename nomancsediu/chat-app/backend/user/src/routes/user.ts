ecc2545b55a69f24eb9c39a586bd5c4625006cb9
nomancsediu <abdnoman720@gmail.com>
2026-05-15 05:43:47 +0600

    feat: expand user routes with new functionality

--- nomancsediu/chat-app/backend/user/src/routes/user.ts
+++ nomancsediu/chat-app/backend/user/src/routes/user.ts
@@ -1,15 +1,16 @@
 import express from 'express';
-import { getAllUsers, getAUser, loginUser, myProfile, verifyUser, updateName } from '../controllers/user.js';
+import { getAllUsers, getAUser, loginUser, myProfile, verifyUser, updateProfile } from '../controllers/user.js';
 import { isAuth } from '../middleware/isAuth.js';
+import { upload } from '../middleware/multer.js';
+import '../config/cloudinary.js';
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/verify", verifyUser);
 router.get("/me", isAuth, myProfile);
-router.get("/user/all", isAuth,getAllUsers);
+router.get("/user/all", isAuth, getAllUsers);
 router.get("/user/:id", getAUser);
-router.put("/user/update/:id", isAuth, updateName);
-
+router.put("/user/update", isAuth, upload.single("avatar"), updateProfile);
 
 export default router;
