ddf5a414ead9e70f5a0bf47bcbabcceaf34b49a7
nomancsediu <abdnoman720@gmail.com>
2026-05-14 23:08:23 +0600

    chore(frontend): update next.js config and dependencies

--- nomancsediu/chat-app/frontend/app/layout.tsx
+++ nomancsediu/chat-app/frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import { AppProvider } from "@/context/AppContext";
 
 export const metadata: Metadata = {
   title: "Alapon",
@@ -12,10 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className="min-h-full flex flex-col">
+        <AppProvider>
+          {children}
+        </AppProvider>
+        <Toaster position="top-center" />
+      </body>
     </html>
   );
 }
