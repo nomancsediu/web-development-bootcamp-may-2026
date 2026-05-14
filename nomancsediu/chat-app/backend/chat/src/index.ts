03243a70b9b8ac9cbeb975a20fd5862e00f2cac2
nomancsediu <abdnoman720@gmail.com>
2026-05-15 05:40:40 +0600

    refactor: improve main index file structure

--- nomancsediu/chat-app/backend/chat/src/index.ts
+++ nomancsediu/chat-app/backend/chat/src/index.ts
@@ -4,16 +4,17 @@ import dotenv from 'dotenv';
 import connectDb from './config/db.js';
 import chatRoutes from './routes/chat.js';
 import './config/cloudinary.js';
+import { app,server } from './config/socket.js';
 
 dotenv.config();
 
 connectDb();
-const app = express();
+
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use('/api/v1', chatRoutes);
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
