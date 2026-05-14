62af1e17675f9863ed36cbb01db957c1c27710cd
nomancsediu <abdnoman720@gmail.com>
2026-05-15 05:44:20 +0600

    refactor: optimize app layout structure

--- nomancsediu/chat-app/frontend/app/layout.tsx
+++ nomancsediu/chat-app/frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { AppProvider } from "@/context/AppContext";
+import { SocketProvider } from "@/context/SocketContext";
 
 export const metadata: Metadata = {
   title: "Alapon",
@@ -16,8 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-full flex flex-col">
-        <AppProvider>
-          {children}
+        <AppProvider> 
+        <SocketProvider>{children}</SocketProvider>
         </AppProvider>
         <Toaster position="top-center" />
       </body>
