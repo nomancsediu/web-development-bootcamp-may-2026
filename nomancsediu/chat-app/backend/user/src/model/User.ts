8aa415e1b74a5a577ef88e5fcf7623e1fc4eb7ac
nomancsediu <abdnoman720@gmail.com>
2026-05-15 05:43:09 +0600

    refactor: restructure user model

--- nomancsediu/chat-app/backend/user/src/model/User.ts
+++ nomancsediu/chat-app/backend/user/src/model/User.ts
@@ -3,6 +3,11 @@ import mongoose, {Document, Schema} from "mongoose";
 export interface IUser extends Document{
     name: string;
     email: string;
+    avatar: {
+        url: string;
+        publicId: string;
+    };
+    isInvisible: boolean;
 }
 
 const schema: Schema<IUser> = new Schema({
@@ -14,7 +19,15 @@ const schema: Schema<IUser> = new Schema({
         type: String,
         required: true,
         unique: true,
-    }
+    },
+    avatar: {
+        url: { type: String, default: "" },
+        publicId: { type: String, default: "" },
+    },
+    isInvisible: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 export const User = mongoose.model<IUser>("User", schema);
